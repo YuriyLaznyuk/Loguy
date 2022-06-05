f7b064303ddc6c11ac5ee464b3268a323031f452
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-06-05 22:14:20 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, {ReactElement} from 'react';
+import './app.scss';
+
+const App = (): ReactElement => {
+	return (
+		<div>
+			<h1 className='app'>Hello Loguy !!!</h1>
+		</div>
+	);
+};
+
+export default App;
