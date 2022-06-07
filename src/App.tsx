953b29671f102f8b066c56762a2b44996a86600c
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-06-07 12:24:47 +0300

    added modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, {ReactElement} from 'react';
 import './app.scss';
+import Header from './components/Header/Header';
+import Support from './components/Supprot/Support';
 
 const App = (): ReactElement => {
 	return (
-		<div>
-			<h1 className='app'>Hello Loguy !!!</h1>
-			<h1 className='app'>Hello World !!!</h1>
+		<div className='app'>
+			<Header />
+			<Support />
 		</div>
 	);
 };
