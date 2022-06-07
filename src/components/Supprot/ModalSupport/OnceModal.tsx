49c0caec87659e325756237684722f02b9dd0fd7
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-06-07 21:52:55 +0300

    added onceModal

--- src/components/Supprot/ModalSupport/OnceModal.tsx
+++ src/components/Supprot/ModalSupport/OnceModal.tsx
@@ -1,9 +1,104 @@
 import React from 'react';
-
-const OnceModal = () => {
+import './onceModal.scss';
+type Props = {
+	method: () => void;
+};
+const OnceModal = ({method}: Props) => {
 	return (
-		<div>
-			<h1>OnceModal</h1>
+		<div className='onceModal'>
+			<span className='onceModal__close' onClick={() => method()}>
+				&#215;
+			</span>
+			<div className='onceModal__title'>Разова допомога</div>
+			<div className='onceModal__input'>
+				<div className='onceModal__input-item'>
+					<div className='onceModal__input-item-text'>
+						Your email<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Please enter your email'
+					/>
+					<div className='onceModal__input-item-text'>
+						First and last name<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Enter your email'
+					/>
+					<div className='onceModal__input-item-text'>
+						First and last name<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Enter your email'
+					/>
+					<div className='onceModal__input-item-text'>
+						First and last name<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Enter your email'
+					/>
+					<div className='onceModal__input-item-text'>
+						Card<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Enter card number'
+					/>
+				</div>
+				<div className='onceModal__input-item'>
+					<div className='onceModal__input-item-text'>
+						Your email<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Please enter your email'
+					/>
+					<div className='onceModal__input-item-text'>
+						First and last name<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Enter your email'
+					/>
+					<div className='onceModal__input-item-text'>
+						First and last name<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Enter your email'
+					/>
+					<div className='onceModal__input-item-text'>
+						First and last name<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Enter your email'
+					/>
+					<div className='onceModal__input-item-text'>
+						Card<span className='app__red'>*</span>
+					</div>
+					<input
+						className='onceModal__input-item-inp'
+						type='text'
+						placeholder='Enter card number'
+					/>
+				</div>
+			</div>
+			<div className='onceModal__button'>
+				<button className='onceModal__button-btn'>Підтримати</button>
+			</div>
 		</div>
 	);
 };
--- src/components/Supprot/Support.tsx
+++ src/components/Supprot/Support.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './support.scss';
+import OnceModal from './ModalSupport/OnceModal';
 
 const Support = () => {
+	const [modal, setModal] = useState({
+		once: false,
+		ukr: false,
+		usd: false,
+		crypto: false,
+	});
+
 	return (
 		<div className='support'>
+			{modal.once && (
+				<OnceModal
+					method={() =>
+						setModal({once: false, ukr: false, usd: false, crypto: false})
+					}
+				/>
+			)}
 			<div className='support__title'>Підтримати</div>
-			<div className='support__item'>Разова допомога</div>
+			<div
+				className={
+					modal.once ? 'support__item support__opacity' : 'support__item'
+				}>
+				<span className={modal.once ? 'support__item-border' : ''}></span>
+				<span
+					onClick={() =>
+						setModal({once: true, ukr: false, usd: false, crypto: false})
+					}>
+					Разова допомога
+				</span>
+			</div>
 			<div className='support__item'>Українські реквізити</div>
 			<div className='support__item'>Долари США (USD/840)</div>
 			<div className='support__item'>Криптовалюта</div>
