45383f33280b0e89d8e89a4a65a3faeeac2e83bc
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-06-08 15:37:51 +0300

    added Founder

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, {ReactElement} from 'react';
 import './app.scss';
 import Header from './components/Header/Header';
 import Support from './components/Supprot/Support';
+import Founder from './components/Founder/Founder';
 
 const App = (): ReactElement => {
 	return (
 		<div className='app'>
 			<Header />
 			<Support />
+			<Founder />
 		</div>
 	);
 };
